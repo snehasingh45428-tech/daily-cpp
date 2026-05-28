0f90adcba6e33bf8b53d3de2ec9e468479166477
unknown <snehasingh45428@gmail.com>
2026-05-28 21:47:06 +0530

    day6: control statements

---
+++ day6.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int marks;
+    cout<<"enter the marks:";
+    cin>> marks;
+    if(marks>=90){
+        cout<<"A\n";
+    } else if( marks>=80 && marks<90){
+        cout<<"B\n";
+    
+    }else{
+        cout<<"C\n";
+    }
+    return 0;
+}
