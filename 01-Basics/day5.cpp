4d751bdf621fc30a5188c16e43db3c2143ca6407
unknown <snehasingh45428@gmail.com>
2026-05-28 20:56:32 +0530

    day5 : Variables and intput output

---
+++ 01-Basics/day5.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int n;
+    cout <<"enter the number:";
+    cin>>n;
+    if (n % 2 == 0)
+    {
+        cout<<"it is even number \n";
+    } else{
+        cout << "it is odd number \n";
+    
+    }
+return 0;
+}
