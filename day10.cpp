cbe20aaf48934966687ba60a2645d130d4c09fad
unknown <snehasingh45428@gmail.com>
2026-05-31 11:39:50 +0530

    Day 10: oddsum

---
+++ day10.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n=50;
+    int oddsum=0;
+    for (int i=1; i<=n; i++){
+        if(i%2!=0){
+            oddsum +=i;
+        
+        }
+        cout<<"oddsum="<<oddsum<<endl;
+
+    }
+    return 0;
+}
