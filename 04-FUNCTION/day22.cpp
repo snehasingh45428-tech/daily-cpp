a5a6352df38cb134ba8091239f471fe28bef1980
unknown <snehasingh45428@gmail.com>
2026-06-01 21:50:16 +0530

    Day 22: double

---
+++ 04-FUNCTION/day22.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+double sum(double a,double b){
+    double s=a+b;
+    return s; 
+} 
+int main(){
+   cout<< sum(10.99 , 5.65)<< endl;
+    return 0;
+
+}
